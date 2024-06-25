f86bff9459e19e281efc744edd499f3a819d25a2
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-06-26 00:29:08 +0600

    added images from the database

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
+import Footer from "./components/Footer"
+import Main from "./components/Main"
+import Sidebar from "./components/Sidebar"
 
 
 function App() {
 
   return (
     <>
-      <div>
- 
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card ">
-        
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Sidebar/>
+     <Main/>
+     <Footer/>
     </>
   )
 }
