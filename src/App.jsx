dc9fd0268d112bb8227ce3770a3af1063ddebd54
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-06-26 04:36:01 +0600

    get data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
-import { useState } from "react"
-import Footer from "./components/Footer"
-import Main from "./components/Main"
-import Sidebar from "./components/Sidebar"
-
+import { useEffect, useState } from "react";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
+import { FaEarthAmericas } from "react-icons/fa6";
 
 function App() {
-  const [showModal, setShowModal] =useState(false)
-  function handleToggleModal (){
-    setShowModal(!showModal)
+  const [apiData, setApiData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  function handleToggleModal() {
+    setShowModal(!showModal);
   }
+
+  useEffect(() => {
+    async function fetchApiData() {
+      const API_URL = import.meta.env.VITE_NASA_API_URL;
+      const NASA_KEY = import.meta.env.VITE_NASA_KEY;
+      const url = `${API_URL}` + `?api_key=${NASA_KEY}`;
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        setApiData(data);
+        console.log("DATA/n", setApiData);
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    // fetchApiData();
+  }, []);
   return (
     <>
-     <Main/>
-    {showModal && <Sidebar handleToggleModal={handleToggleModal}/>}
-     <Footer handleToggleModal={handleToggleModal}/>
+      {apiData ? (
+        <Main />
+      ) : (
+        <div className="">
+          <FaEarthAmericas />
+        </div>
+      )}
+      {showModal && <Sidebar handleToggleModal={handleToggleModal} />}
+      <Footer handleToggleModal={handleToggleModal} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
